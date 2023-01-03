6ffdca64122f26efae1097d29dbbedac5c1bfd69
brycemihal <bryce.mihal@gmail.com>
2023-01-03 16:14:59 -0700

    added accordion cams

--- js/slc/camera_slc.js
+++ js/slc/camera_slc.js
@@ -1,19 +1,72 @@
+var bcc_cam_links = ['https://udottraffic.utah.gov/1_devices/aux14605.jpeg',
+    'https://udottraffic.utah.gov/1_devices/aux16212.jpeg',
+    'https://udottraffic.utah.gov/1_devices/aux16213.jpeg',
+    'https://udottraffic.utah.gov/1_devices/aux16215.jpeg',
+    'https://udottraffic.utah.gov/1_devices/aux16216.jpeg',
+    'https://udottraffic.utah.gov/1_devices/aux18040.jpeg',
+    'http://udottraffic.utah.gov/1_devices/SR-190%20MP%2015%2095%20SL.gif']
 
-var cLinks = ['https://udottraffic.utah.gov/1_devices/aux14605.jpeg',
+var lcc_cam_links = ['https://udottraffic.utah.gov/1_devices/aux16607.jpeg',
+    'https://udottraffic.utah.gov/1_devices/aux14604.jpeg',
+    'https://udottraffic.utah.gov/1_devices/aux16265.jpeg',
+    'https://udottraffic.utah.gov/1_devices/aux16267.jpeg',
+    'https://udottraffic.utah.gov/1_devices/aux16269.jpeg',
+    'https://udottraffic.utah.gov/1_devices/aux16270.jpeg',
     'https://udottraffic.utah.gov/1_devices/aux17227.jpeg',
-    'https://udottraffic.utah.gov/1_devices/aux17226.jpeg',
-    'https://udottraffic.utah.gov/1_devices/aux16215.jpeg']
+    'https://udottraffic.utah.gov/1_devices/aux17228.jpeg',
+    'https://udottraffic.utah.gov/1_devices/aux17226.jpeg']
 
-var linkNames = ['Logan Canyon Summit', 'Logan Canyon - Right Hand Fork', 'Sardine Canyon Summit', 'Blask Smith Fork - Left Hand']
+document.getElementById('bcc_cam0').src = bcc_cam_links[0];
+document.getElementById('bcc_cam1').src = bcc_cam_links[1];
+document.getElementById('bcc_cam2').src = bcc_cam_links[2];
+document.getElementById('bcc_cam3').src = bcc_cam_links[3];
+document.getElementById('bcc_cam4').src = bcc_cam_links[4];
+document.getElementById('bcc_cam5').src = bcc_cam_links[5];
+document.getElementById('bcc_cam6').src = bcc_cam_links[6];
 
-// document.getElementById('cName0').innerHTML = linkNames[0]
-document.getElementById('cImage0').src = cLinks[0];
+document.getElementById('lcc_cam0').src = lcc_cam_links[0];
+document.getElementById('lcc_cam1').src = lcc_cam_links[1];
+document.getElementById('lcc_cam2').src = lcc_cam_links[2];
+document.getElementById('lcc_cam3').src = lcc_cam_links[3];
+document.getElementById('lcc_cam4').src = lcc_cam_links[4];
+document.getElementById('lcc_cam5').src = lcc_cam_links[5];
+document.getElementById('lcc_cam6').src = lcc_cam_links[6];
+document.getElementById('lcc_cam7').src = lcc_cam_links[7];
+document.getElementById('lcc_cam8').src = lcc_cam_links[8];
 
-// document.getElementById('cName1').innerHTML = linkNames[1]
-document.getElementById('cImage1').src = cLinks[1];
 
-// document.getElementById('cName2').innerHTML = linkNames[2]
-document.getElementById('cImage2').src = cLinks[2]
+// LCC
+// WASATCH BLVD @ LITTLE COTTONWOOD
+// https://udottraffic.utah.gov/1_devices/aux16607.jpeg
+// INTERSECTION OF HWY 209 & HWY 210
+// https://udottraffic.utah.gov/1_devices/aux14604.jpeg
+// LITTLE COTTONWOOD MP 5.96
+// https://udottraffic.utah.gov/1_devices/aux16265.jpeg
+// LITTLE COTTONWOOD MP 7.4
+// https://udottraffic.utah.gov/1_devices/aux16267.jpeg
+// LITTLE COTTONWOOD MP 8.7
+// https://udottraffic.utah.gov/1_devices/aux16269.jpeg
+// LITTLE COTTONWOOD @ WHITE PINE PARKING MP 9.2
+// https://udottraffic.utah.gov/1_devices/aux16270.jpeg
+// LITTLE COTTONWOOD MP 9.7
+// https://udottraffic.utah.gov/1_devices/aux17227.jpeg
+// LITTLE COTTONWOOD @ ALTA BYPASS MP 10.95
+// https://udottraffic.utah.gov/1_devices/aux17228.jpeg
+// LITTLE COTTONWOOD @ ALTA MP 12.16
+// https://udottraffic.utah.gov/1_devices/aux17226.jpeg
 
-// document.getElementById('cName3').innerHTML = linkNames[3]
-document.getElementById('cImage3').src = cLinks[3];
+// BCC
+// Wasatch Blvd @ Big Cottonwood Canyon
+// https://udottraffic.utah.gov/1_devices/aux14605.jpeg
+// Big Cottonwood Dogwood MP 4.1
+// https://udottraffic.utah.gov/1_devices/aux16212.jpeg
+// Big Cottonwood @ S Curves MP 6.38
+// https://udottraffic.utah.gov/1_devices/aux16213.jpeg
+// Big Cottonwood @ Cardiff Fork MP 10.74
+// https://udottraffic.utah.gov/1_devices/aux16215.jpeg
+// Big Cottonwood @ Silver Fork MP 12.54
+// https://udottraffic.utah.gov/1_devices/aux16216.jpeg
+// Big Cottonwood @ Solitude MP 14.5
+// https://udottraffic.utah.gov/1_devices/aux18040.jpeg
+// Big Cottonwood @ Brighton Circle MP 16
+// http://udottraffic.utah.gov/1_devices/SR-190%20MP%2015%2095%20SL.gif
